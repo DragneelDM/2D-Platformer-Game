34d013210c0e142686aa329a03ad38598c26601e
Abishek Anbazhagan <designer.abishek@gmail.com>
2024-03-14 15:08:37 +0530

    Set Levels

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
+    [SerializeField] Button button;
     [SerializeField, Range(0,10)] int SceneIndex;
+    private void Start() {
+        if(button != null)
+            button.onClick.AddListener(Play);
+    }
     public void Play(){
         SceneManager.LoadScene(SceneIndex);
     }
