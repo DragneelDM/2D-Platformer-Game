960278995077b217afc108d9aac50d636a404c41
Abishek Anbazhagan <designer.abishek@gmail.com>
2024-03-14 14:33:35 +0530

    HEHEHe

---
+++ Assets/Scripts/Gunner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunner : MonoBehaviour
+{
+    [SerializeField] Vector3[] patrolPoints;
+    float elapsedTime;
+    [SerializeField] float lerpTime = 5f;
+    bool facingLeft = true;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime < lerpTime)
+        {
+            float t = elapsedTime / lerpTime;
+            transform.position = Vector3.Lerp(patrolPoints[0], patrolPoints[1], t);
+        }
+        else
+        {
+            // Reset the timer when reaching the destination
+            elapsedTime = 0f;
+            SwapPoints();
+        }
+    }
+
+    void SwapPoints()
+    {
+        Vector3 temp = patrolPoints[0];
+
+        transform.localScale = facingLeft == true ? new Vector3(1,1,1) : new Vector3(-1,1,1);
+        facingLeft = !facingLeft;
+
+        patrolPoints[0] = patrolPoints[1];
+        patrolPoints[1] = temp;
+
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void reload(){
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void Play(){
+        FindObjectOfType<Image>().sprite = null;
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
